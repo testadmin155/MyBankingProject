02fe55c4ffc8521523ec02958fb5797b72dc9779
Anees <skanees007@gmail.com>
2023-05-27 23:34:59 +0200

    Forth commit into develope branch

--- src/MyBanking/PersonalLoan.java
+++ src/MyBanking/PersonalLoan.java
@@ -5,6 +5,7 @@ public class PersonalLoan {
 	public static void main(String[] args) {
 		System.out.println("Personal loan");
 		System.out.println("this is flexi loan");
+		System.out.println("this is a Fixed Loan");
 
 	}
 
